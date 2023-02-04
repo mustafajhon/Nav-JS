3131ea154539dc5d930c933aa427c0c517e30bbf
mustafa <mustafanaiby@gmail.com>
2023-02-04 20:13:11 +0330

    ddd

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Home from "./Components/home/home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/home/home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+function Home() {
+  return (
+    <>
+      <nav>
+        <div className="container">
+          <div className="logo">
+            <h1>Mustafa</h1>
+          </div>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Service</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Home;
